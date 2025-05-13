10db252bf6cc0fb0646f55f6ce0cc5454a0431b7
Joel Bello <joel8bl@gmail.com>
2025-05-13 10:29:56 +0200

    removing offenses

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "products#index"
 
-
   # get "/products", to: "products#index"
-
   # get "/products/new", to: "products#new"
   # post "/products", to: "products#create"
-
   # get "/products/:id", to: "products#show"
-
   # get "/products/:id/edit", to: "products#edit"
   # patch "/products/:id", to: "products#update"
   # put "/products/:id", to: "products#update"
-
   # delete "/products/:id", to: "products#destroy"
-
   resources :products
-
 end
